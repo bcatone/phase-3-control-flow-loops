a352f41c8f80a8ca3a11c8e62cddb8eb67300ebd
Brittany Laura Catone <bcatone@outlook.com>
2023-01-09 11:11:07 -0500

    commit tree e6b03686d43f832d7da7edec1675da7f0e8d4126

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 def happy_new_year
   # your code here
+  i = 10
+  while i > 0
+    puts i
+    i -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,20 @@ end
 
 def fizzbuzz_printer
   # your code here
+  i = 1
+  until i > 100
+    puts fizzbuzz(i)
+    i += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed_string = ""
+  i = 0
+  until i == str.length
+    reversed_string = str[i] + reversed_string
+    i += 1
+  end
+  reversed_string
 end
